c52c5f0fac054aa1a88e81724cf1bb2bdbd65d60
Miguel7113 <miguelndakwe11@gmail.com>
2024-11-18 22:15:17 +0300

    get

--- server/app.py
+++ server/app.py
@@ -1,38 +1,84 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "Index for Bakery/Baked Goods API"
+
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    if bakery:
+        bakery_dict = bakery.to_dict()
+        response = make_response(
+            jsonify(bakery_dict),
+            200
+        )
+    else:
+        response = make_response(
+            {"error": "Bakery not found"},
+            404
+        )
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [
+        baked_good.to_dict()
+        for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all()
+    ]
+
+    response = make_response(
+        jsonify(baked_goods),
+        200
+    )
+    return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    if baked_good:
+        baked_good_dict = baked_good.to_dict()
+        response = make_response(
+            jsonify(baked_good_dict),
+            200
+        )
+    else:
+        response = make_response(
+            {"error": "No baked goods found"},
+            404
+        )
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
